da2587cd359579856586181189d36fe6e019e80b
dawn_lch <chung4111@naver.com>
2024-08-05 14:49:47 +0900

    first

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -17,6 +17,6 @@ public class OrderServiceTest {
         memberService.join(member);
         Order order=orderService.createOrder(memberId, "itemA",10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
-    }//
+    }//dd
 
 }
