c5187abb537414f27fb368612d07aca4775c24e5
dawn_lch <chung4111@naver.com>
2024-08-07 22:33:55 +0900

    스프링으로 변환

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig=new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        //MemberService memberService = new MemberServiceImpl();
+//        /*AppConfig appConfig=new AppConfig();
+//        MemberService memberService = appConfig.memberService();*/
+        //스프링 컨테이너 생성 후 Bean 등록
+        ApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+        //Bean 가져오기
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);//ctrl+Alt+v
         memberService.join(member);
 
